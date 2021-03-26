21eede940dbbba7a2227274bbff498db59d4f1d3
SzHHard <korenkov_misha8611111@mail.ru>
2021-03-26 20:07:43 +0300

    попробовал добавить supertest на post

--- server/api.js
+++ server/api.js
@@ -12,9 +12,19 @@ db.all('SELECT * FROM tasks', (err, rows) => {
 });
 
 const tasks = ['testString'];
-router.get('/tasks', (req, res, next) => {   //new
+
+router.get('/tasks', (req, res, next) => {   
     res.send(tasks);
 });
 
+router.post('/tasks', (req, res, next) => {
+    const newElement = {name: req.query.name}; 
+    if(newElement) {
+        tasks.push(newElement);
+        res.status(201).send(newElement);
+    }  else {
+        res.status(400).send();
+    }
+});
 
 module.exports = router;
