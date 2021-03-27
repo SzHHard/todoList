d68bfba40e7f31b986daed6c85e993e88e0f262f
szhhard <korenkov_misha8611111@mail.ru>
2021-03-27 17:52:41 +0300

    added sqlite3 and set up db module

--- server/api.js
+++ server/api.js
@@ -2,15 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('./sqliteDb');
 
-db.all('SELECT * FROM tasks', (err, rows) => {
-    if(err){
-        throw err;
-    }
-    else {
-        console.log(rows);
-    }
-});
-
 const tasks = ['testString'];
 
 router.get('/tasks', (req, res, next) => {   
---
+++ server/db.js
@@ -0,0 +1,52 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('../db.sqlite');
+
+/**
+ * Creates a new Tasks Table
+ * @param {Callback} done 
+ */
+function createTable(done) {
+    db.run('CREATE TABLE tasks (id INTEGER PRIMARY KEY, content TEXT)', done);
+}
+
+/**
+ * Get All Tasks
+ * @param {(err: Error, tasks: []string) => {}} done 
+ */
+function getAllTasks(done) {
+    db.all('SELECT * FROM tasks', done);
+}
+
+/**
+ *  Get a task by id
+ * @param {number} id 
+ * @param {(err: Error, task: string) => {}} done 
+ */
+function getTask(id, done) {
+    db.get('SELECT * FROM tasks WHERE id = $id', {
+        $id: id,
+    }, done)
+}
+/**
+ * Insert a Task into the table
+ * @param {Task Text} task 
+ * @param {Callback} done 
+ */
+function insertTask(task, done) {
+    db.run(`INSERT INTO tasks (id, content) VALUES (1, "$task")`, { $task: task }, done);
+}
+
+/**
+ * Delete a Task from the table by Id
+ * @param {Task Id} id 
+ * @param {Callback} done 
+ */
+function deleteTask(id, done) {
+    db.run(`DELETE FROM tasks WHERE id = $id`, {
+        $id: id
+    }, done)
+}
+
+
+module.exports = { createTable, insertTask, deleteTask, getTask, getAllTasks }
+
