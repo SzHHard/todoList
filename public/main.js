f3870413f6e1d8c589e6220dd90e2aabe26dea5c
SzHHard <korenkov_misha8611111@mail.ru>
2021-03-30 22:53:15 +0300

    Made the GET request fully functional and rewrote the main.js so that it's more close to MVC model

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
-var list = document.getElementById("todo-list");
-var input_list = document.getElementById("input-list");
-
+const list = document.getElementById("todo-list");
+const input_list = document.getElementById("input-list");
+const howManyLeft = document.getElementById('how-many-left');
 
 const server = 'http://localhost:3000/api';
 const urlForGetAllRequest = server + '/tasks';
@@ -16,111 +16,143 @@ const sendRequest = () => {
 }, networkError => console.log(networkError.message));
 }
 
-  // sendRequest().then(data => {
-  //     console.log(data);
-  //     for(let i = 0; i < data.length; i++) {
-  //       if(data[i].active === 1) {
-  //         Cards.all.push(data[i].content);
-  //         Cards.active.push(data[i].content);
-  //       } 
-  //       if(data[i].active === 0) {
-  //         Cards.all.push(data[i].content);
-  //         Cards.completed.push(data[i].content);
-  //       }
-  //     }
-  // }).catch(err => console.log(err));
+  sendRequest().then(data => {
+      console.log(data);
+      for(let i = 0; i < data.length; i++) {
+        if(data[i].active === 1) {
+          View.incrementActiveCounter();
+          const li1 = Controller.createNewListItem(data[i].content);
+          let checkbx = Controller.createCheckboxForLi(li1);
+          Controller.createDeleteButtonForLi(li1);
+        } 
+        if(data[i].active === 0) {
+         
+           const li1 = Controller.createNewListItem(data[i].content);
+           li1.firstElementChild.style.opacity = '0.2';
+           li1.firstElementChild.style.textDecoration = 'line-through';
+           let checkbx = Controller.createCheckboxForLi(li1);
+           checkbx.checked = true;
 
-//window.onload = sendRequest;
-
-
-let Cards = {
-  _all: [],
-  _active: [],
-  _completed: [],
-  get all() {
-    return this._all;
-  },
-  get active() {
-    return this._active;
-  },
-  get completed() {
-    return this._completed;
-  },
-};
+           Controller.createDeleteButtonForLi(li1);
+        }
+      }
+  }).catch(err => console.log(err));
 
 
 
-let activeCounter = 0;
-const howManyLeft = document.getElementById('how-many-left');
+
+
+const Model = {
+    Cards: {
+        activeCounter: 0,
+        _all: [],
+        _active: [],
+        _completed: [],
+        get all() {
+            return this._all;
+        },
+        get active() {
+            return this._active;
+        },
+        get completed() {
+            return this._completed;
+        },
+    },
+    pushNewActiveTask(task) {
+        this.Cards.all.push(task);
+        this.Cards.active.push(task);
+    }
+
+}
+
+const View = {
+    incrementActiveCounter() {
+        Model.Cards.activeCounter++;
+        howManyLeft.innerHTML = Model.Cards.activeCounter;
+    },
+    decrementActiveCounter() {
+        Model.Cards.activeCounter--;
+        howManyLeft.innerHTML = Model.Cards.activeCounter;
+    },
+    renderNewListItem(taskText) {
+        let inp = taskText;
+        input_list.value = '';
+        let li1 = document.createElement('li');
+        list.appendChild(li1);
+        li1.innerHTML = `<p>${inp}<p>`;
+        return li1;
+    }
+}
+
+const Controller = {
+    createNewListItem(taskText) {
+        const li1 = View.renderNewListItem(taskText);
+        Model.pushNewActiveTask(li1);
+        return li1;
+    },
+
+    createCheckboxForLi(li1) {                                   // Это не MVC, но пока так
+        let checkbx = document.createElement('input');
+        checkbx.type = 'checkbox';
+        checkbx.className = 'checkbx';
+        li1.appendChild(checkbx);
+        checkbx.addEventListener('change', () => {
+            if (checkbx.checked) {
+                Model.Cards.completed.push(li1);
+                Model.Cards.active.splice(Model.Cards.active.indexOf(li1), 1);
+                li1.firstElementChild.style.opacity = '0.2';
+                li1.firstElementChild.style.textDecoration = 'line-through';
+                View.decrementActiveCounter();
+
+            } else {
+                Model.Cards.active.push(li1);
+                Model.Cards.completed.splice(Model.Cards.completed.indexOf(li1), 1);
+                li1.firstElementChild.style.opacity = '1';
+                li1.firstElementChild.style.textDecoration = 'none';
+                View.incrementActiveCounter();
+            }
+
+        })
+        return checkbx;
+    },
+
+    createDeleteButtonForLi(li1) {
+        let button1 = document.createElement('button');
+        li1.appendChild(button1);
+        button1.className = 'del-but';
+        //button1.innerText = 'del';
+        button1.addEventListener('click', () => {
+            if (li1.childNodes[2].checked) {
+                Model.Cards.all.splice(Model.Cards.all.indexOf(li1), 1);
+                Model.Cards.completed.splice(Model.Cards.completed.indexOf(li1), 1); // может она не попала в completed? вроде, попала
+
+            } else {
+                Model.Cards.all.splice(Model.Cards.all.indexOf(li1), 1);
+                Model.Cards.active.splice(Model.Cards.active.indexOf(li1), 1);
+                View.decrementActiveCounter();
+            }
+
+            li1.remove();
+
+        })
+    }
+}
+
+// одна из идей - создать в controller функцию создания checkbox со встроенным eventlistener
 
 input_list.addEventListener("keydown", (event) => {
 
-  if (event.code === "Enter") {
-    activeCounter++;
-    howManyLeft.innerHTML = activeCounter;
-    let inp = input_list.value;
-    input_list.value = '';
-    let li1 = document.createElement('li');
-    list.appendChild(li1);
-    li1.innerHTML = `<p>${inp}<p>`;
-    //
+    if (event.code === "Enter") {
 
-    fetch('/tasks', { method: 'POST', body: JSON.stringify({ text: inp }) }).then(res => {
-      if (res.status != 201) {
-        throw Error('Unable to create Task on Server')
-      }
-    })
-    Cards.all.push(li1);
-    Cards.active.push(li1);
+        View.incrementActiveCounter();
 
+        const li1 = Controller.createNewListItem(input_list.value); // li1 - new created item
+        // 
+        //
+        let checkbx = Controller.createCheckboxForLi(li1);
 
-
-
-    //
-
-    //
-    let checkbx = document.createElement('input');
-    checkbx.type = 'checkbox';
-    checkbx.className = 'checkbx';
-    li1.appendChild(checkbx);
-    checkbx.addEventListener('change', () => {
-      if (checkbx.checked) {
-        Cards.completed.push(li1);
-        Cards.active.splice(Cards.active.indexOf(li1), 1);
-        li1.firstElementChild.style.opacity = '0.2';
-        li1.firstElementChild.style.textDecoration = 'line-through'; //сюда ткнуть PUT запрос на active: false
-        activeCounter--;
-
-      } else {
-        Cards.active.push(li1);
-        Cards.completed.splice(Cards.completed.indexOf(li1), 1);   
-        li1.firstElementChild.style.opacity = '1';
-        li1.firstElementChild.style.textDecoration = 'none';
-        activeCounter++;                                                //сюда ткнуть PUT запрос на active: true
-      }
-      howManyLeft.innerHTML = activeCounter;
-    })
-
-    let button1 = document.createElement('button');
-    li1.appendChild(button1);
-    button1.className = 'del-but';
-    //button1.innerText = 'del';
-    button1.addEventListener('click', () => {
-      if (li1.childNodes[2].checked) {
-        Cards.all.splice(Cards.all.indexOf(li1), 1);
-        Cards.completed.splice(Cards.completed.indexOf(li1), 1); // может она не попала в completed? вроде, попала
-
-      } else {
-        Cards.all.splice(Cards.all.indexOf(li1), 1);
-        Cards.active.splice(Cards.active.indexOf(li1), 1);
-        activeCounter--;
-        howManyLeft.innerHTML = activeCounter;
-      }
-
-      li1.remove(); // сюда ткнуть DELETE запрос 
-                                    
-    })
-  }
+        Controller.createDeleteButtonForLi(li1);
+    }
 });
 
 const buttonAll = document.createElement('button');
@@ -140,52 +172,52 @@ tableFooter.appendChild(buttonCompleted);
 tableFooter.appendChild(buttonClearCompleted);
 
 buttonAll.addEventListener('click', () => {
-  for (let i = list.children.length - 1; i > 0; i--) {
-    list.children[i].remove();
-  }
-  for (let i = 0; i < Cards.all.length; i++) {
-    list.appendChild(Cards.all[i]);
-  }
-  buttonAll.classList.add('btnclicked');
-  buttonCompleted.classList.remove('btnclicked');
-  buttonActive.classList.remove('btnclicked');
+    for (let i = list.children.length - 1; i > 0; i--) {
+        list.children[i].remove();
+    }
+    for (let i = 0; i < Model.Cards.all.length; i++) {
+        list.appendChild(Model.Cards.all[i]);
+    }
+    buttonAll.classList.add('btnclicked');
+    buttonCompleted.classList.remove('btnclicked');
+    buttonActive.classList.remove('btnclicked');
 });
 
 buttonActive.addEventListener('click', () => {
-  for (let i = list.children.length - 1; i > 0; i--) {
-    list.children[i].remove();
-  }
-  for (let i = 0; i < Cards.active.length; i++) {
-    list.appendChild(Cards.active[i]);
-  }
-  buttonActive.classList.add('btnclicked');
-  buttonAll.classList.remove('btnclicked');
-  buttonCompleted.classList.remove('btnclicked');
+    for (let i = list.children.length - 1; i > 0; i--) {
+        list.children[i].remove();
+    }
+    for (let i = 0; i < Model.Cards.active.length; i++) {
+        list.appendChild(Model.Cards.active[i]);
+    }
+    buttonActive.classList.add('btnclicked');
+    buttonAll.classList.remove('btnclicked');
+    buttonCompleted.classList.remove('btnclicked');
 });
 
 buttonCompleted.addEventListener('click', () => {
-  for (let i = list.children.length - 1; i > 0; i--) {
-    list.children[i].remove();
-  }
-  for (let i = 0; i < Cards.completed.length; i++) {
-    list.appendChild(Cards.completed[i]);
-  }
-  buttonCompleted.classList.add('btnclicked');
-  buttonAll.classList.remove('btnclicked');
-  buttonActive.classList.remove('btnclicked');
+    for (let i = list.children.length - 1; i > 0; i--) {
+        list.children[i].remove();
+    }
+    for (let i = 0; i < Model.Cards.completed.length; i++) {
+        list.appendChild(Model.Cards.completed[i]);
+    }
+    buttonCompleted.classList.add('btnclicked');
+    buttonAll.classList.remove('btnclicked');
+    buttonActive.classList.remove('btnclicked');
 });
 
 buttonClearCompleted.addEventListener('click', () => {
-  for (let i = list.children.length - 1; i > 0; i--) {
-    list.children[i].remove();
-  }
-  for (let i = 0; i < Cards.active.length; i++) {
-    list.appendChild(Cards.active[i]);
-  }
-  // перед этим из массива all нужно убрать все приколы, которые есть и здесь
-  Cards._all = Cards.all.filter(obj => {
-    return (Cards.completed.indexOf(obj) === -1);
-  })
-  Cards._completed = [];
+    for (let i = list.children.length - 1; i > 0; i--) {
+        list.children[i].remove();
+    }
+    for (let i = 0; i < Model.Cards.active.length; i++) {
+        list.appendChild(Model.Cards.active[i]);
+    }
+    // перед этим из массива all нужно убрать все приколы, которые есть и здесь
+    Model.Cards._all = Model.Cards.all.filter(obj => {
+        return (Model.Cards.completed.indexOf(obj) === -1);
+    })
+    Model.Cards._completed = [];
 });
 
