d9f589eb98eb10bef57c7e6b396fa39ddcae8bd0
SzHHard <korenkov_misha8611111@mail.ru>
2021-04-27 12:48:38 +0300

    Changed the tests for them to work in sequelize and passed 9/10

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -1,14 +1,14 @@
 const db = require("../db");
-//const Task = db.task;
+const Task = db.task;
 const Op = db.Sequelize.Op;
 
 // Create and Save a new Task
 exports.create = async (req, res) => {
 
-    console.log(req.body);
+   
     // Validate request
     if (!req.body.text) {
-       // console.log('aye');
+      
        
         res.status(400).send({
             message: "Content can not be empty!"
@@ -20,7 +20,7 @@ exports.create = async (req, res) => {
     //    active: true
     };
     try {
-        let data = await db.task.create(task)
+        let data = await Task.create(task)
         
         res.status(201).send(data);
     } catch (err) {
@@ -34,10 +34,10 @@ exports.create = async (req, res) => {
 // Retrieve all Tasks from the database.
 exports.findAll = async (req, res) => {
     try {
-      let tasks = await db.task.findAll()
+      let tasks = await Task.findAll()
       res.send(tasks)
     } catch(err) {
-       // console.log(err);
+        console.log(err);
         res.status(500).send({
             message: err.message || 'Some error occurred while retrieving tasks'
         });
@@ -46,17 +46,93 @@ exports.findAll = async (req, res) => {
 
 // Find a single Task with an id
 exports.findOne = (req, res) => {
+    const id = req.params.id;
 
+    Task.findByPk(id)
+        .then(data => {
+           if(data === null) {
+            res.status(404).send();
+           } 
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(404).send({
+                message: 'Error retrieving Task with id=' + id
+            })
+        })
 };
 
 // Update a Task by the id in the request
 exports.update = (req, res) => {
-
+    const id = req.params.id;
+  
+    if(req.query.active){
+        Task.update(
+        {active: req.query.active}, 
+        {where: { id: id } }
+        )
+        .then(num => {
+            console.log(num)
+            if (num == 1) {
+              res.send();
+            } else {
+              res.status(400).send({
+                message: `Cannot update Task with id=${id}. Maybe Task was not found or req.body is empty!`
+              });
+            }
+          })
+          .catch(err => {
+            res.status(400).send({
+              message: "Error updating Task with id=" + id
+            });
+          });
+        } else {
+            if(req.query.content) {
+                Task.update(
+                    {content: req.query.content}, 
+                    {where: { id: id } }
+                    )
+                    .then(num => {
+                        if (num !== 0) {
+                          res.send();
+                        } else {
+                          res.status(400).send({
+                            message: `Cannot update Task with id=${id}. Maybe Task was not found or req.body is empty!`
+                          });
+                        }
+                      })
+                      .catch(err => {
+                        res.status(400).send({
+                          message: "Error updating Task with id=" + id
+                        });
+                      });
+            }
+        }
 };
 
 // Delete a Task with the specified id in the request
 exports.delete = (req, res) => {
+    const id = req.params.id;
 
+    Task.destroy({
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Task was deleted successfully!"
+          });
+        } else {
+          res.status(404).send({
+            message: `Cannot delete Task with id=${id}. Maybe Task was not found!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Could not delete Task with id=" + id
+        });
+      });
 };
 
 // Delete all Tasks from the database.
--- test/tasks.js
+++ test/tasks.js
@@ -10,14 +10,8 @@ const { sequelize } = require('../server/db');
 app.use('/', routerApi);
 
 describe('todoList api v1', function () {
-    before((done) => {
-        
-       db.sequelize.sync({force: true}).then(() => {
-           console.log('Drop and re-sync db.');
-       })
-        
-        done();
-        
+    before(() => {
+        return db.sequelize.sync({ force: true })
     })
 
     describe('/tasks', function () {
@@ -42,7 +36,6 @@ describe('todoList api v1', function () {
                     request(app)
                         .get('/')
                         .expect((res) => {
-                         //   console.log(res);
                             assert.strictEqual(res.body[0].content, "hello, world")
                             assert.strictEqual(res.body[0].id, 1)
                         })
@@ -51,7 +44,7 @@ describe('todoList api v1', function () {
 
         });
 
-        it('on PUT with query passed should update task content', (done) => {
+        it('on PUT with content passed should update task content', (done) => {
             request(app)
                 .post('/')
                 .send({ text: "before" })
@@ -59,28 +52,22 @@ describe('todoList api v1', function () {
                 .expect('Content-Type', /json/)
                 .expect(201)
                 .end((err, res) => {
-                    if (err) return done(err)
-                    let id = -1;
-
-                    db.getMaxIdFromTable((err, row) => {
-                        id = row['MAX(id)'];
-                        request(app)
-                            .put(`/${id}?content=after`)
-                            .set('Accept', 'application/json')
-                            .expect(200)
-                            .end((err) => {
-                                if (err) return done(err)
-                                request(app)
-                                    .get(`/${id}`)
-                                    .set('Accept', 'application/json')
-                                    .expect(200)
-                                    .expect((res) => {
-                                        JSON.stringify(res);
-                                        assert.strictEqual(res.body.content, 'after');
-                                    })
-                                    .end(done)
-                            })
-                    })
+                    let id = res.body.id
+                    request(app)
+                        .put(`/${id}?content=after`)
+                        .set('Accept', 'application/json')
+                        .expect(200)
+                        .end((err) => {
+                            if (err) return done(err)
+                            request(app)
+                                .get(`/${id}`)
+                                .set('Accept', 'application/json')
+                                .expect(200)
+                                .expect((res) => {
+                                    assert.strictEqual(res.body.content, 'after');
+                                })
+                                .end(done)
+                        })
                 })
 
         })
@@ -92,103 +79,84 @@ describe('todoList api v1', function () {
                 .post('/')
                 .send({ text: "hello, world" })
                 .set('Accept', 'application.json')
-                .end(() => {
-                    let id = -1;
-
-                    db.getMaxIdFromTable((err, row) => {
-                        id = row['MAX(id)'];
-
-                        request(app)
-                            .get(`/${id}`)
-                            .set('Accept', 'application/json')
-                            .expect('Content-Type', /json/)
-                            .expect(200)
-                            .end(done)
-                    })
+                .end((err, res) => {
+                    let id = res.body.id;
+                    request(app)
+                        .get(`/${id}`)
+                        .set('Accept', 'application/json')
+                        .expect('Content-Type', /json/)
+                        .expect(200)
+                        .end(done)
                 })
         })
-
         it('on DELETE should remove a task', (done) => {
             request(app)
                 .post('/')
                 .send({ text: "DELETE test" })
-                .end(() => {
-                    db.countRows((err, row) => {
-                        let lengthAfterAdd = row['COUNT(*)'];
-                        db.getMaxIdFromTable((err, row) => {
-                            let id = row['MAX(id)'];
+                .end((err, res) => {
+                    let id = res.body.id;
+                    request(app)
+                        .delete(`/${id}`)
+                        .expect(204)
+                        .end(() => {
                             request(app)
-                                .delete(`/${id}`)
-                                .expect(204)
-                                .end(() => {
-                                    db.countRows((err, row) => {
-                                        let newLength = row['COUNT(*)'];
-                                        request(app)
-                                            .get('/')
-                                            .expect(200)
-                                            .expect((res) => {
-                                                assert.strictEqual(newLength + 1, lengthAfterAdd);
-                                            })
-                                            .end(done)
-                                    });
-                                })
-                        })
-                    });
-
+                                .get(`/${id}`)
+                                .expect(404)
+                                .end(done)
+                        });
                 })
         })
         it('on PUT should update the task to completed', (done) => {
             request(app)
                 .post('/')
                 .send({ text: "putTest" })
-                .end(() => {
-                    db.getMaxIdFromTable((err, row) => {
-                        let id = row['MAX(id)'];
-                        request(app)
-                            .put(`/${id}?active=false`)
-                            .expect(200)
-                            .expect(() => {
-                                db.getTask(id, (err, row) => {
-                                    assert.strictEqual(row.active, 0);
-                                });
-                            })
-                            .end(done)
-                    });
-                })
-        });
+                .end((err, res) => {
+                    let id = res.body.id;
+                    request(app)
+                        .put(`/${id}?active=false`)
+                        .expect(200)
+                        .end(() => {
+                            request(app)
+                                .get(`/${id}`)
+                                .expect((res) => {
+                                    assert.strictEqual(res.body.active, false)
+                                })
+                                .end(done)
+                        })
+                });
+        })
 
         it('on PUT should update the task to active', (done) => {
             request(app)
                 .post('/')
                 .send({ text: "hello, world" })
-                .end(() => {
-                    db.getMaxIdFromTable((err, row) => {
-                        let id = row['MAX(id)'];
-                        request(app)
-                            .put(`/${id}?active=true`)
-                            .expect(200)
-                            .expect(() => {
-                                db.getTask(id, (err, row) => {
-                                    assert.strictEqual(row.active, 1);
-                                });
-                            })
-                            .end(done)
-                    });
-                })
-        });
-
-
-
+                .end((err, res) => {
+                    let id = res.body.id;
+                    request(app)
+                        .put(`/${id}?active=true`)
+                        .expect(200)
+                        .end(() => {
+                            request(app)
+                                .get(`/${id}`)
+                                .expect((res) => {
+                                    assert.strictEqual(res.body.active, true)
+                                })
+                                .end(done)
+                        })
+                });
+        })
+    
+    
         it('on GET should fail if passed an invalid task id', (done) => {
             request(app)
                 .get('/1999922949')
-                .expect(400)
+                .expect(404)
                 .end(done)
         })
-        it('on DELETE should return 400 if invalid task id', (done) => {
+        it('on DELETE should return 404 if invalid task id', (done) => {
             request(app)
                 .delete('/1938427')
-                .expect(400)
+                .expect(404)
                 .end(done)
         })
         it('on PUT should return 400 if invalid task id', (done) => {
@@ -197,6 +165,8 @@ describe('todoList api v1', function () {
                 .expect(400)
                 .end(done)
         })
-    })
-});
+    });
+
+})
+
 
